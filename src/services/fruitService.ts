dc5a53a1bddaefc5fce635c17c14cc55df4094c9
Bruce Lan <50816749+bruce647@users.noreply.github.com>
2024-10-29 15:08:55 -0400

    Update fruitService.ts

--- src/services/fruitService.ts
+++ src/services/fruitService.ts
@@ -10,7 +10,7 @@ import { Fruit } from "../types/Fruit";
 export const getFruits = async (): Promise<Fruit[]> => {
   try {
     //vite dynamic import
-    const fruits = await import('../data/fruits.json');
+    const fruits = await import("../data/fruits.json");
     return fruits.default;
   } catch (error) {
     console.error("Error fetching fruits:", error);
